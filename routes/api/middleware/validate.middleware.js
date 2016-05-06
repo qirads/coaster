edd10f7cb17049cc7b6fb64619f3cd4ffa6673c8
yaacovCR <yaacovCR@gmail.com>
2016-05-06 09:55:33 -0400

    Add arrays to validate middleware

--- routes/api/middleware/validate.middleware.js
+++ routes/api/middleware/validate.middleware.js
@@ -10,67 +10,90 @@ module.exports = function(schema) {
     
     validateFields(req.body, schema, details, '');
     if (_.size(details)) {
+      console.log(details);
       return next(createError(400, 'Invalid request', { details: details }));
     }
     next();
   };
-    
-  function validateFields(body, schema, details, prefix) {
+  
+  function validateFields(fields, schema, details, prefix) {
     _.forEach(schema, function(fieldSchema) {
-      if (fieldSchema.type === 'object') {
-        if (body[fieldSchema.name]) {
-          validateFields(body[fieldSchema.name], fieldSchema.schema, details, prefix + fieldSchema.name + '.');
-        } else if (fieldSchema.required) {
-          details.push({
-            fieldName: prefix + fieldSchema.name,
-            required: fieldSchema.required,
-            expectedFieldType: fieldSchema.type,
-            schema: fieldSchema.schema,
-            errors: ['missing required field']
-          });            
-        }
-      } else {
-        validateField(body, fieldSchema, details, prefix);
-      }
+      checkFieldSchema(fields[fieldSchema.name], fieldSchema, details, prefix);      
     });
-    checkExtraneousFields(body, schema, details, prefix);
+    checkExtraneousFields(fields, schema, details, prefix);
+  }
+
+  function checkFieldSchema(field, fieldSchema, details, prefix) {
+    if (_.isArray(fieldSchema.type) === _.isArray(field)) {
+      if (_.isArray(field)) {
+        _.forEach(field, function(individualField, index) {
+          var individualFieldSchema = _.merge({}, fieldSchema, { type: fieldSchema.type[0]});
+          checkEachField(individualField, individualFieldSchema, details, prefix, index);
+        });
+      } else {
+        checkEachField(field, fieldSchema, details, prefix);
+      }
+    } else {
+      var errors = [(_.isArray(field) ? 'received' : 'expected') + ' array of values instead of simple value'];
+      addToDetails(field, fieldSchema, details, prefix, errors);
+    }
+  }
+
+  function checkEachField(field, fieldSchema, details, prefix, index) {
+    if (fieldSchema.type === 'object') {
+      if (field) {
+        if (typeof field === 'object') {
+          validateFields(
+            field, fieldSchema.schema, details,
+            prefix + fieldSchema.name + (index === undefined ? '.' : '[' + index + '].'));          
+        } else {
+          addToDetails(field, fieldSchema, details, prefix, ['expected object instead of simple value'], index);          
+        }
+      } else if (fieldSchema.required) {
+        addToDetails(field, fieldSchema, details, prefix, ['missing required field'], index);
+      }
+    } else {
+      validateField(field, fieldSchema, details, prefix, index);
+    }
   }
   
   function checkExtraneousFields(body, schema, details, prefix) {
     var allowedFields = _.map(schema, function(fieldSchema) { return fieldSchema.name; });
     var extraneousFields = _.omit(body, allowedFields);
     _.forEach(extraneousFields, function(value, key) {
-      details.push({
-        fieldName: prefix + key,
-        fieldValue: value,
-        fieldType: typeof value,
-        error: 'unrecognized field'
-      });
+      addToDetails(field, {}, details, prefix, 'unrecognized field', index);
     });
   }
-    
-  function validateField(body, fieldSchema, details, prefix) {
-    var fieldErrors = [];
-    if (fieldSchema.required && !body[fieldSchema.name]) {
-      fieldErrors.push('missing required field');
+  
+  function validateField(field, fieldSchema, details, prefix, index) {
+    var errors = [];
+    if (fieldSchema.required && !field) {
+      errors.push('missing required field');
     }
-    if (body[fieldSchema.name] && fieldSchema.type != typeof body[fieldSchema.name]) {
-      fieldErrors.push('unexpected field type');
+    if (field && fieldSchema.type != typeof field) {
+      errors.push('unexpected field type');
     }
-    if (body[fieldSchema.name] && fieldSchema.allowedValues && !_.includes(fieldSchema.allowedValues, body[fieldSchema.name])) {
-      fieldErrors.push('value not allowed');
+    if (field && fieldSchema.allowedValues && !_.includes(fieldSchema.allowedValues, field)) {
+      errors.push('value not allowed');
     }
-    if (_.size(fieldErrors)) {
-      details.push({
-        fieldName: prefix + fieldSchema.name,
-        fieldValue: body[fieldSchema.name],
-        fieldType: typeof body[fieldSchema.name],
-        required: fieldSchema.required,
-        expectedFieldType: fieldSchema.type,
-        allowedValues: fieldSchema.allowedValues,
-        errors: fieldErrors
-      });
+    if (_.size(errors)) {
+      addToDetails(field, fieldSchema, details, prefix, errors, index);
     }
   }
-      
+  
+  function addToDetails(field, fieldSchema, details, prefix, errors, index) {
+    var detail = {
+      fieldName: prefix + fieldSchema.name + (index === undefined ? '' : '[' + index + ']'),
+      fieldValue: field,
+      fieldType: typeof field,
+      expectedFieldType: fieldSchema.type,
+    };
+    
+    if (fieldSchema.required) { _.merge(detail, { required: fieldSchema.required }); }
+    if (fieldSchema.allowedValues) { _.merge(detail, { allowedValues: fieldSchema.allowedValues }); }
+    if (fieldSchema.schema) { _.merge(detail, { schema: fieldSchema.schema }); }
+    
+    details.push(_.merge(detail, { errors: errors }));
+  }
+  
 }
