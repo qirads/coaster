8a795d889a23912fe061ec6ee39aa0657b6187f8
yaacovCR <yaacovCR@gmail.com>
2017-04-23 01:59:07 -0400

    [chore] offline localhost on windows spun out to separate pkg
    node-offline-localhost now takes care of this.

--- utils/index.js
+++ utils/index.js
@@ -1,30 +1,12 @@
 'use strict';
 
-var dns = require('dns');
 var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
 
 var utils = {
-  offline: offline,
   forEachSubdir: forEachSubdir
 };
-
-function offline() {
-  if (process.platform === 'win32') {
-    if (!dns.lookupWithAddrConfigSupport) {
-      dns.lookupWithAddrConfigSupport = dns.lookup;
-      dns.lookup = lookupWithoutAddrConfigSupport;
-    }
-  }  
-}
-
-function lookupWithoutAddrConfigSupport(hostname, options, callback) {
-  if (typeof options !== 'function' && options !== null && options.hints & dns.ADDRCONFIG) {
-    options.hints &= ~dns.ADDRCONFIG;
-  } 
-  return dns.lookupWithAddrConfigSupport(hostname, options, callback);
-}
 
 function forEachSubdir(dir, fn) {
   
