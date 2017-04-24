36a10381128ebe3246250725cb405ee3b3e186d1
yaacovCR <yaacovCR@gmail.com>
2017-04-24 09:42:09 -0400

    [refactor] add wrapper for express-restify-mongoose

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,14 @@
 
 module.exports = function(app, config, clients) {
 
-  var router = require('express').Router();
-  var restify = require('express-restify-mongoose');
-  var createError = require('http-errors');  
+  var restifyWrapper = require('./restify.wrapper')(app);
   var _ = require('lodash');
-  
-  var defaults = {
-    allowRegex: false,
-    private: ['__v']    
-  };
-  
-  defaults.onError = function (err, req, res, next) {
-    next(createError(err.statusCode));
-  };
-
-  defaults.outputFn = function(req, res) {
-
-    function renameId(result) {
-      if (result._id) {
-        result.id = result._id;
-        delete result._id;
-      }
-    }
-
-    if (req.erm.result) {
-      if (_.isArray(req.erm.result)) {
-        _.forEach(req.erm.result, renameId);
-      } else {
-        renameId(req.erm.result);
-      }
-      res.status(req.erm.statusCode).json(req.erm.result)
-    } else {
-      res.sendStatus(req.erm.statusCode)
-    }
-  };
 
   // setup api routes
   _.forEach(['user', 'session', 'study', 'search'], function(endpoint) {
-    restify.serve(
-      app,
+    restifyWrapper.serve(
       require('./' + endpoint + '.model')(config, clients),
-      _.merge({}, defaults, require('./' + endpoint + '.config')(app, config, clients), customizer));
+      require('./' + endpoint + '.config')(app, config, clients));
   });
   
   // return 404 for any unmatched API routes
@@ -54,9 +21,5 @@ module.exports = function(app, config, clients) {
   app.use(function(req, res) {
     res.redirect(301, '/');
   });
-
-  function customizer(a, b) {
-    return (_.isArray(a)) ? a.concat(b) : undefined;
-  }
 
 };
---
+++ routes/restify.wrapper.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = function(app) {
+
+  var router = require('express').Router();
+  var restify = require('express-restify-mongoose');
+  var createError = require('http-errors');  
+  var _ = require('lodash');
+  
+  var defaults = {
+    allowRegex: false,
+    private: ['__v']    
+  };
+  
+  defaults.onError = function (err, req, res, next) {
+    next(createError(err.statusCode));
+  };
+
+  defaults.outputFn = function(req, res) {
+
+    function renameId(result) {
+      if (result._id) {
+        result.id = result._id;
+        delete result._id;
+      }
+    }
+
+    if (req.erm.result) {
+      if (_.isArray(req.erm.result)) {
+        _.forEach(req.erm.result, renameId);
+      } else {
+        renameId(req.erm.result);
+      }
+      res.status(req.erm.statusCode).json(req.erm.result)
+    } else {
+      res.sendStatus(req.erm.statusCode)
+    }
+  };
+
+  function serve(model, routeConfig) {
+    restify.serve(app, model, _.merge({}, defaults, routeConfig, customizer));
+  }
+
+  return {
+    serve
+  }
+
+  function customizer(a, b) {
+    return (_.isArray(a)) ? a.concat(b) : undefined;
+  }
+
+};
